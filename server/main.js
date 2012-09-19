f0b0254a64e595c7adbf5134c690dd282966d9d6
zhang chen <kingdemoncc@gmail.com>
2012-09-19 17:29:19 +0800

    web woker
    ha  ha  ha  ha

--- server/main.js
+++ server/main.js
@@ -1,5 +1,16 @@
 var conn = require("./gameObj/Connection.js");
 var IO = require('socket.io').listen(9005);
 
-conn.connect(IO);
-
+//conn.connect(IO);
+IO.sockets.on('connection', function(_socket) {
+	console.log("connection 启动...........");
+	_socket.on("login", function(_data) {
+		console.log(_data);
+		_socket.emit("login", "success"+_data);
+	});
+	_socket.on('disconnect', function() {
+		//下线.
+		console.log("客户端已退出");
+	});
+	_socket.emit("loginInit", "success");
+});
